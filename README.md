03bd4ded95fb68c241cb5fa21c62f031aff03e4e
Logan Monson <scarletfedora@gmail.com>
2022-03-17 01:07:33 -0700

    struggling with the quiz pat of the quiz

--- README.md
+++ README.md
@@ -5,4 +5,14 @@ This is a browser-based quiz game, testing your knowledge of coding and its lang
 Some big questions i need to figure out before i start:
     --How will i store/pull my different questions?
     --Order of elements in HTML
-    
+
+
+## Day_One
+The most important thing to do is make sure all the different parts of the web page work individually before I linked it all together. So the first part to make was the timer. I was able to make it work, but it wasnt triggered by event listener...
+
+## Day_Two
+Today I'm going to work on the multiple-choice questions. For this it needs to do two things:
+    -Cycle through each question upon input choice
+    -Reward choosing the correct answer/punish the wrong answer
+
+I am considering putting the question in an array: the question itself, 4 choices to choose from, and the answer. If i can assign each variable in the array to semantic elements on the HTML, the user can choose which array element is correct. At which point, once the correct answer is chosen, it wil switch to the new array.
--- script.js
+++ script.js
@@ -1,6 +1,57 @@
+//Timer variables
 var timerEl = document.querySelector(".timer");
-var start = document.querySelector("#start")
+var start = document.querySelector("#start");
 var timeLeft = 70;
+
+//Quiz variables
+var question = document.querySelector("#question");
+var answerOne = document.querySelector("#a");
+var answerTwo = document.querySelector("#b");
+var answerThree = document.querySelector("#c");
+var answerFour = document.querySelector("#d");
+var correctAnswer = document.querySelector("#answer");
+
+var questionnaire = [
+    {
+        prompt: "which one is a?",
+        a: "a",
+        b: "b",
+        c: "c",
+        d: "d",
+        answer: "a",
+    },
+    {
+        prompt: "which one is 7?",
+        a: "4",
+        b: "bt",
+        c: "7",
+        d: "d",
+        answer: "c",   
+    },
+];
+
+function quizGame() {
+    for (var i=0; i<questionnaire.length; i++) {
+        console.log(questionnaire[i].prompt);
+        console.log(questionnaire[i].a);
+        console.log(questionnaire[i].b);
+        console.log(questionnaire[i].c);
+        console.log(questionnaire[i].d);
+    }
+};
+
+//var questionOne = ["what are bees?", "Aliens", "Plants", "Insects", "Rocks", "Insects"];
+//var questionTwo = ["Pineapple on pizza?", "Yes!", "No!", "Ew, what?", "I'm calling the cops...", "Yes!"];
+//var questionnaire = [questionOne, questionTwo];
+
+//var optionOne = document.querySelector('#a1')
+//var optionTwo = document.querySelector('#a2')
+//var optionThree = document.querySelector('#a3')
+//var optionFour = document.querySelector('#a4')
+
+console.log(questionnaire)
+console.log(questionnaire[0]);
+console.log(questionnaire[1]);
 
 
 //function startCountdown(event) {
@@ -10,18 +61,18 @@ var timeLeft = 70;
         timeLeft--;
 
         if (timeLeft > 1) {
-            console.log(timeLeft + " seconds!");
+            //console.log(timeLeft + " seconds!");
             timerEl.textContent = timeLeft;  
         } else if (timeLeft === 1) {
-            console.log(timeLeft + " second!");
+           // console.log(timeLeft + " second!");
             timerEl.textContent = timeLeft;
         } else if (timeLeft === 0) {
             clearInterval(timerInterval);
-            console.log("Game Over!");
+            //console.log("Game Over!");
             timerEl.textContent ="Game Over!";
         }
 
     }, 1000);    
 //}
-
+quizGame()
 //start.addEventListener("click", startCountdown)
